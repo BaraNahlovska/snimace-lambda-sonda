60af1b77d2f6de30fe381ab655b8911c59b31863
barca <bara.nahlovska@seznam.cz>
2025-06-20 09:17:30 +0200

    add(master):new  switch

--- Snimace_lambda_sonda/Program.cs
+++ Snimace_lambda_sonda/Program.cs
@@ -7,39 +7,35 @@ class Program
         Console.WriteLine("Zadej typ motoru: malý (do 1.3l), střední (1.4 - 1.9l), velký (nad 2.0l)");
         string typMotoru = Console.ReadLine().ToLower();
 
-        double minOtacky = 0, maxOtacky = 0;
-        double minMAF = 0, maxMAF = 0;
-        double minLambda = 0.98, maxLambda = 1.02;
+        double minMAF = 0; 
+        double maxMAF = 0;
 
-        if (typMotoru == "malý" || typMotoru == "maly")
+
+        switch (typMotoru)
         {
-            Console.WriteLine("Hodnoty nastaveny pro motor do objemu 1.3l)");
-            minOtacky = 700;
-            maxOtacky = 900;
-            minMAF = 2.5;
-            maxMAF = 4.5;
+            case "malý":
+            case "maly":
+                Console.WriteLine("Hodnoty nastaveny pro motor do objemu 1.3l)");
+                minMAF = 2.5;
+                maxMAF = 4.5; 
+                break;
+            case "střední":
+            case "stredni":
+                Console.WriteLine("Hodnoty nastaveny pro motory o objemu 1.4 - 1.9l");
+                minMAF = 3.5;
+                maxMAF = 6.0;
+                break;
+            case "velký":
+            case "velky":
+                Console.WriteLine("Hodnota nastaveny pro motory o objemu nad 2.0l");
+                minMAF = 5.0;
+                maxMAF = 8.0;
+                break;
+            default:
+                Console.WriteLine("Neplatný vstup. Zadej malý, stžední nebo velký"); 
+                return;
         }
-        else if (typMotoru == "střední" || typMotoru == "stredni")
-        {
-            Console.WriteLine("Hodnoty nastaveny pro motory o objemu 1.4 - 1.9l");
-            minOtacky = 750;
-            maxOtacky = 950;
-            minMAF = 3.5;
-            maxMAF = 6.0;
-        }
-        else if (typMotoru == "velký" || typMotoru == "velky")
-        {
-            Console.WriteLine("Hodnota nastaveny pro motory o objemu nad 2.0l");
-            minOtacky = 750;
-            maxOtacky = 1000;
-            minMAF = 5.0;
-            maxMAF = 8.0;
-        }
-        else
-        {
-            Console.WriteLine("Neplatný vstup");
-            return;
-        }
+        
 
         Console.WriteLine("Zadej otáčky motoru (např. 850):");
         int otacky = Convert.ToInt32(Console.ReadLine());
@@ -50,31 +46,39 @@ class Program
         Console.WriteLine("Zadej hodnotu pro lambda sondu (např. 1.00)");
         double lambda = Convert.ToDouble(Console.ReadLine());
 
-        if (otacky >= minOtacky && otacky <= maxOtacky)
-        {
-            Console.WriteLine("Otáčky jsou v normě.");
-        }
-        else
-        {
-            Console.WriteLine("Otáčky jsou mimo rozsah!");
-        }
+        bool otackyOk = otacky >= 650 && otacky <= 950;
+        bool lambdaOk = lambda >= 0.98 && lambda <= 1.02;
+        bool mafOk = maf >= minMAF && maf <= maxMAF;
 
-        if (maf >= minMAF && maf <= maxMAF)
-        {
-            Console.WriteLine("Hodnota MAF je v normě");
-        }
-        else
-        {
-            Console.WriteLine("Hodnota MAF je mimo rozsah!");
-        }
+        Console.WriteLine("\n--- VYHODNOCENÍ ---"); 
 
-        if (lambda >= minLambda && lambda <= maxLambda)
+
+        if (otackyOk && lambdaOk && mafOk)
         {
-            Console.WriteLine("Lambda sonda je v normě");
+            Console.WriteLine("Všechny hodnoty jsou v normě.");
         }
         else
         {
-            Console.WriteLine("Hondnota lambda sondy je mimo rozsah!");
+ 
+            if (!otackyOk)
+                Console.WriteLine("Otáčky jsou mimo rozsah volnoběhu!"); 
+            if (!lambdaOk) 
+                { Console.WriteLine("Hodnota lambda sondy vykazuje odchylku"); 
+            if (!mafOk && otackyOk && lambdaOk)
+            {
+                    if (maf > maxMAF)
+                        Console.WriteLine("Hodnota MAF je vysoká!");
+                    else if (maf < minMAF)
+                        Console.WriteLine("Hodnota MAF je nízká!");
+            }
+
+            if (!otackyOk && !lambdaOk && !mafOk)  
+                Console.WriteLine("Všechny hodnoty mimo rozsah");
+
+
+            }
+
+
         }
     }
 }
